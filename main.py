462b3cd9a5f692cf0acb408d241d575e6afe5bd1
Evgeniy Shesterikov <67272160+evgen4ikrus@users.noreply.github.com>
2023-03-12 16:22:28 +0300

    save messages history to file

--- main.py
+++ main.py
@@ -1,27 +1,32 @@
 import asyncio
 import logging
+from datetime import datetime
 
+import aiofiles
 from environs import Env
 
 logger = logging.getLogger(__name__)
 
 
-async def save_message(host, port):
+async def save_message(host, port, file_name):
     reader, writer = await asyncio.open_connection(host, port)
     while True:
         chunk = await reader.readline()
-        message = chunk.decode()
+        message = f'[{datetime.now().strftime("%Y-%m-%d, %H:%M")}] {chunk.decode()}'
         print(message)
+        async with aiofiles.open(file_name, mode='a') as file:
+            await file.write(message)
 
 
 def main():
+    message_history_file = 'message_history.txt'
     logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
                         level=logging.DEBUG)
     env = Env()
     env.read_env()
     host = env('CONNECTING_HOST')
     port = env('CONNECTING_PORT')
-    asyncio.run(save_message(host, port))
+    asyncio.run(save_message(host, port, message_history_file))
 
 
 if __name__ == '__main__':
