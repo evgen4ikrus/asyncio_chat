8c740dad50803db02b25a14aee0126d2684888b2
Evgeniy Shesterikov <67272160+evgen4ikrus@users.noreply.github.com>
2023-03-16 19:28:47 +0300

    setup logging

--- send_messages.py
+++ send_messages.py
@@ -1,26 +1,42 @@
 import asyncio
-
+import logging
 from environs import Env
+from datetime import datetime
+
+logger = logging.getLogger(__name__)
 
 
-async def send_message(host, port, token):
-    reader, writer = await asyncio.open_connection(host, port)
-    message = f'{token}\n'
-    writer.write(message.encode())
+async def write_to_socket(writer, message):
+    writer.write(f'{message}\n\n'.encode())
     await writer.drain()
-    while True:
-        message = f'{input()}\n\n'
-        writer.write(message.encode())
-        await writer.drain()
+
+
+async def send_message(host, port, token, message):
+    reader, writer = await asyncio.open_connection(host, port)
+    if token:
+        chunk = await reader.readline()
+        outgoing_message = f'[{datetime.now().strftime("%Y-%m-%d, %H:%M")}] {chunk.decode().rstrip()}'
+        logger.info(outgoing_message)
+        await write_to_socket(writer, token)
+        logger.info(f'Sent message: {token}')
+        chunk = await reader.readline()
+        incoming_message = f'[{datetime.now().strftime("%Y-%m-%d, %H:%M")}] {chunk.decode().rstrip()}'
+        logger.info(incoming_message)
+    chunk = await reader.readline()
+    incoming_message = f'[{datetime.now().strftime("%Y-%m-%d, %H:%M")}] {chunk.decode().rstrip()}'
+    logger.info(incoming_message)
+    await write_to_socket(writer, message)
+    logger.info(f'Sent message: {message}')
 
 
 def main():
     env = Env()
     env.read_env()
+    logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s %(message)s', level=logging.DEBUG)
     chat_host = env('CHAT_HOST', 'minechat.dvmn.org')
     chat_port = env.int('CHAT_PORT', 5050)
     chat_token = env('CHAT_TOKEN')
-    asyncio.run(send_message(chat_host, chat_port, chat_token))
+    asyncio.run(send_message(chat_host, chat_port, chat_token, 'Привет'))
 
 
 if __name__ == '__main__':
