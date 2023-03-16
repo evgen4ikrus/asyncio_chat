71bc7f7f02989f2920793eda49cb6c8575a8264a
Evgeniy Shesterikov <67272160+evgen4ikrus@users.noreply.github.com>
2023-03-16 20:47:59 +0300

    add script for user registration

--- send_messages.py
+++ send_messages.py
@@ -1,8 +1,8 @@
 import asyncio
 import json
 import logging
-from datetime import datetime
 
+import aiofiles
 from environs import Env
 
 logger = logging.getLogger(__name__)
@@ -13,25 +13,37 @@ async def write_to_socket(writer, message):
     await writer.drain()
 
 
+async def register_user(reader, writer):
+    await write_to_socket(writer, '\n')
+    await reader.readline()
+    response = await reader.readline()
+    user = json.loads(response)
+    nickname = user.get("nickname")
+    account_hash = user.get("account_hash")
+    logger.info(f'Регистрация успешно завершена, nickname: {nickname}, account_hash: {account_hash}')
+    async with aiofiles.open('.env', mode='a') as file:
+        await file.write(f'CHAT_TOKEN={account_hash}\n')
+    logger.info('В файл `.env` добавлен CHAT_TOKEN=account_hash')
+
+
 async def send_message(host, port, token, message):
     reader, writer = await asyncio.open_connection(host, port)
     if token:
-        response = await reader.readline()
-        outgoing_message = f'[{datetime.now().strftime("%Y-%m-%d, %H:%M")}] {response.decode().rstrip()}'
-        logger.info(outgoing_message)
+        await reader.readline()
         await write_to_socket(writer, token)
-        logger.info(f'Sent message: {token}')
         response = await reader.readline()
-        if not json.loads(response):
-            logger.error('Невалидный токен')
-            return 
-        incoming_message = f'[{datetime.now().strftime("%Y-%m-%d, %H:%M")}] {response.decode().rstrip()}'
-        logger.info(incoming_message)
-    response = await reader.readline()
-    incoming_message = f'[{datetime.now().strftime("%Y-%m-%d, %H:%M")}] {response.decode().rstrip()}'
-    logger.info(incoming_message)
+        user = json.loads(response)
+        if user:
+            logger.info(f'Вы зашли под ником: {user.get("nickname")}')
+        else:
+            logger.info('Невалидный токен, сейчас будет произведена регистрация')
+            await register_user(reader, writer)
+    else:
+        logger.info('Вы не зарегистрированы, сейчас будет произведена регистрация')
+        await reader.readline()
+        await register_user(reader, writer)
     await write_to_socket(writer, message)
-    logger.info(f'Sent message: {message}')
+    logger.info(f'Вы отправили сообщения в чат: {message}')
 
 
 def main():
@@ -40,7 +52,7 @@ def main():
     logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s %(message)s', level=logging.DEBUG)
     chat_host = env('CHAT_HOST', 'minechat.dvmn.org')
     chat_port = env.int('CHAT_PORT', 5050)
-    chat_token = env('CHAT_TOKEN')
+    chat_token = env('CHAT_TOKEN', '')
     asyncio.run(send_message(chat_host, chat_port, chat_token, 'Привет'))
 
 
