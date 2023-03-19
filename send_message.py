424153746f1f72397723d769adaedb9fa8ae0528
Evgeniy Shesterikov <67272160+evgen4ikrus@users.noreply.github.com>
2023-03-19 10:35:25 +0300

    add CLI options to sender script

--- send_message.py
+++ send_message.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import json
 import logging
@@ -8,12 +9,23 @@ from environs import Env
 logger = logging.getLogger(__name__)
 
 
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('message', help='отправляемое в чат сообщение')
+    parser.add_argument('--host', help='хост чата')
+    parser.add_argument('--port', help='порт чата для отправки сообщения')
+    parser.add_argument('--token', help='токен пользователя чата')
+    parser.add_argument('--username', help='username для регистрации в чате')
+    args = parser.parse_args()
+    return args.message, args.host, args.port, args.token, args.username
+
+
 async def write_to_socket(writer, message):
-    writer.write(f'{message}\n\n'.encode())
+    writer.write(f'{message}\n'.encode())
     await writer.drain()
 
 
-async def authorise(reader, writer, token):
+async def authorise(reader, writer, token, username):
     await write_to_socket(writer, token)
     response = await reader.readline()
     user = json.loads(response)
@@ -21,31 +33,31 @@ async def authorise(reader, writer, token):
         logger.info(f'Вы зашли под ником: {user.get("nickname")}')
     else:
         logger.info('Невалидный токен, сейчас будет произведена регистрация')
-        await register_user(reader, writer)
+        await register_user(reader, writer, username)
 
 
-async def register_user(reader, writer):
-    await write_to_socket(writer, '\n')
+async def register_user(reader, writer, username):
     await reader.readline()
+    await write_to_socket(writer, username)
     response = await reader.readline()
     user = json.loads(response)
     nickname = user.get("nickname")
     account_hash = user.get("account_hash")
     logger.info(f'Регистрация успешно завершена, nickname: {nickname}, account_hash: {account_hash}')
     async with aiofiles.open('.env', mode='a') as file:
-        await file.write(f'CHAT_TOKEN={account_hash}\n')
+        await file.write(f'USER_CHAT_TOKEN={account_hash}\n')
     logger.info('В файл `.env` добавлен CHAT_TOKEN=account_hash')
 
 
-async def send_message(host, port, token, message):
+async def send_message(host, port, token, message, username):
     reader, writer = await asyncio.open_connection(host, port)
+    await reader.readline()
     if token:
-        await reader.readline()
-        await authorise(reader, writer, token)
+        await authorise(reader, writer, token, username)
     else:
         logger.info('Вы не зарегистрированы, сейчас будет произведена регистрация')
-        await reader.readline()
-        await register_user(reader, writer)
+        await write_to_socket(writer, '')
+        await register_user(reader, writer, username)
     await write_to_socket(writer, message)
     logger.info(f'Вы отправили сообщения в чат: {message}')
 
@@ -54,11 +66,12 @@ def main():
     env = Env()
     env.read_env()
     logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s %(message)s', level=logging.DEBUG)
-    chat_host = env('CHAT_HOST', 'minechat.dvmn.org')
-    chat_port = env.int('CHAT_PORT', 5050)
-    chat_token = env('CHAT_TOKEN', '')
-    message = 'При\nвет\n \n \n'.replace("\n", "\\n")
-    asyncio.run(send_message(chat_host, chat_port, chat_token, message))
+    message, possible_host, possible_port, possible_token, possible_username = get_args()
+    chat_host = possible_host or env('CHAT_HOST', 'minechat.dvmn.org')
+    sending_message_chat_port = possible_port or env.int('SENDING_MESSAGE_CHAT_PORT', 5050)
+    user_chat_token = possible_token or env('USER_CHAT_TOKEN', '')
+    chat_username = possible_username or env('CHAT_USERNAME', '')
+    asyncio.run(send_message(chat_host, sending_message_chat_port, user_chat_token, message, chat_username))
 
 
 if __name__ == '__main__':
